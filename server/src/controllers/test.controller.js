1e921fe7f95ea7ee8849e1acd8ba17c8d6a7e98e
Lê Đặng Trường Đạt <datldt@viettel.com.vn>
2023-11-04 16:45:13 +0700

    update project

--- server/src/controllers/test.controller.js
+++ server/src/controllers/test.controller.js
@@ -17,7 +17,6 @@ const fileWriter = (filename, data) => {
 };
 
 const fetchData = async (url) => {
-  console.log("Crawling data...");
   // make http call to url
   let response = await axios(url).catch((err) => console.error(err));
 
@@ -28,45 +27,61 @@ const fetchData = async (url) => {
   return response;
 };
 
-const getHtml = async (links, type = "description") => {
-  if (type == "description") {
-    for (let i = 0; i < links.length; i++) {
-      const data = await fetchData(`${links[i]}`).then((res) => {
-        const html = res?.data || "";
+const getHtml = async (links) => {
+  let result = "";
+  for (let i = 0; i < links.length; i++) {
+    const data = await fetchData(`${links[i]}`).then((res) => {
+      const html = res?.data || "";
 
-        const $ = cheerio.load(html, { decodeEntities: false });
+      const $ = cheerio.load(html, { decodeEntities: false });
 
-        $("body").find("style").remove();
-        $("body").find("script").remove();
-        $("body").find("link").remove();
-        $("body").find("a").remove();
+      $("body").find("style").remove();
+      $("body").find("script").remove();
+      $("body").find("link").remove();
+      $("body").find("a").remove();
 
-        const data = $("body").text().replace(/\s+/g, " ");
+      const data = $("body").text().replace(/\s+/g, " ");
 
-        fileWriter(`./description.txt`, data + "\n\n\n");
-      });
-    }
-  } else if (type == "background") {
-    for (let i = 0; i < links.length; i++) {
-      const data = await fetchData(`${links[i]}`).then((res) => {
-        const html = res?.data || "";
-
-        const $ = cheerio.load(html, { decodeEntities: false });
-
-        $("body").find("style").remove();
-        $("body").find("script").remove();
-        $("body").find("link").remove();
-        $("body").find("a").remove();
-
-        const data = $("body").text().replace(/\s+/g, " ");
-
-        fileWriter(`./background.txt`, data + "\n\n\n");
-      });
-    }
+      result = result + data + "\n\n\n";
+    });
   }
+  return result;
 };
 
-const processDocument = async () => {
+const extractTextFromPDF = async (filePath) => {
+  const data = fs.readFileSync(filePath);
+
+  const pdfData = new Uint8Array(data);
+
+  const pdfDocument = await pdfjs.getDocument({ data: pdfData }).promise;
+
+  const numPages = pdfDocument.numPages;
+
+  const textContent = [];
+
+  for (let pageNumber = 1; pageNumber <= numPages; pageNumber++) {
+    const page = await pdfDocument.getPage(pageNumber);
+    const content = await page.getTextContent();
+    const pageText = content.items.map((item) => item.str).join(" ");
+    textContent.push(pageText);
+  }
+
+  return textContent.join("\n\n\n");
+};
+const getFileContent = async (files) => {
+  let result = "";
+
+  for (const file of files) {
+    const filePath = file.path;
+
+    const text = await extractTextFromPDF(filePath);
+    result += text + "\n\n\n";
+  }
+
+  return result;
+};
+
+const processDocument = async (description, background) => {
   const questions = [
     "Given the provided background and the provided product features, create claims for potential patentable IP",
     "From all the provided content, find all the trademark usages, suggest potential trademarks, and describe who trademark can project you and your company",
@@ -74,16 +89,36 @@ const processDocument = async () => {
     "Define what trade secrets are and how they apply to the project, Offer guidance on securing trade secrets",
   ];
 
+  let context = "";
+
+  if (description.length > 0) {
+    context =
+      context +
+      "Here is Description (please upload your document(s) or specify web page link(s) that have your product features or product description): \n\n\n" +
+      description;
+  }
+
+  if (background.length > 0) {
+    context =
+      context +
+      "\n\n\n" +
+      "Here is Background (Optional- please upload your document/pitch desk or specify web page links that have your product background and/or your mission statement): \n\n\n" +
+      background;
+  }
+
+  console.log(context);
+
   let answers = [];
 
   for (let i = 0; i < questions.length; i++) {
     const text = questions[i];
 
     let chatGPTMessage;
-    try {
-      const description = readFile(path.join(__dirname, "/result.txt"));
 
-      chatGPTMessage = await chatGPT(description, `${text}`);
+    try {
+      chatGPTMessage = await chatGPT(context, `${text}`);
+
+      console.log(chatGPTMessage);
 
       answers.push(chatGPTMessage);
     } catch (error) {
@@ -94,103 +129,59 @@ const processDocument = async () => {
 };
 
 export const handleFormSubmit = async (req, res) => {
-  const files = req.files || [];
+  const files = req.files.files || [];
 
   const inputLinks = req.body.links || [];
 
-  const bFiles = req.bFiles || [];
+  const bFiles = req.files.bFiles || [];
 
-  const inputBLinks = req.body.BLinks || [];
+  const inputBLinks = req.body.bLinks || [];
 
-  try {
-    fs.writeFileSync(path.join(__dirname, "/result.txt"), "", "utf8");
-  } catch (error) {
-    console.error("Error writing the file:", error);
-  }
+  let description = "";
+
+  let background = "";
 
   if (typeof inputLinks == "string") {
+    // one link
     let arr = [];
     arr.push(inputLinks);
-    await getHtml(arr);
+    description = await getHtml(arr);
   } else {
-    await getHtml(inputLinks);
+    // multiple links
+    description = await getHtml(inputLinks);
   }
 
   if (typeof inputBLinks == "string") {
     let arr = [];
     arr.push(inputBLinks);
-    await getHtml(arr, "background");
+    background = await getHtml(arr);
   } else {
-    await getHtml(inputBLinks, "background");
+    background = await getHtml(inputBLinks);
   }
 
   let answers = [];
 
-  if (files.length == 0) {
-    answers = await processDocument();
-    return res.status(200).json({
-      message: "Files uploaded and processed successfully.",
-      data: answers,
-    });
-  } else {
-    for (let i = 0; i < files.length; i++) {
-      const filePath = files[i].path;
-
-      fs.promises
-        .readFile(filePath)
-        .then((data) => {
-          const pdfData = new Uint8Array(data);
-
-          pdfjs
-            .getDocument({ data: pdfData })
-            .promise.then(function (pdfDocument) {
-              const numPages = pdfDocument.numPages;
-              const textPromises = [];
-
-              for (let pageNumber = 1; pageNumber <= numPages; pageNumber++) {
-                textPromises.push(
-                  pdfDocument.getPage(pageNumber).then(function (page) {
-                    return page.getTextContent();
-                  })
-                );
-              }
-
-              return Promise.all(textPromises);
-            })
-            .then(async (pages) => {
-              const text = pages
-                .map(function (page) {
-                  return page.items
-                    .map(function (item) {
-                      return item.str;
-                    })
-                    .join(" ");
-                })
-                .join("\n");
-
-              fileWriter(`./result.txt`, text + "\n\n\n");
-
-              answers = await processDocument();
-
-              return res.status(200).json({
-                message: "Files uploaded and processed successfully.",
-                data: answers,
-              });
-
-              fs.promises
-                .unlink(filePath)
-                .then(() => {})
-                .catch((error) => {
-                  console.error(error);
-                });
-            })
-            .catch(function (error) {
-              console.error(error);
-            });
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+  if (files.length > 0) {
+    try {
+      const f = await getFileContent(files);
+      description = description + f;
+    } catch (error) {
+      console.error(error);
     }
   }
+  if (bFiles.length > 0) {
+    try {
+      const b = await getFileContent(bFiles);
+      background = background + b;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  answers = await processDocument(description, background);
+
+  return res.status(200).json({
+    message: "Files uploaded and processed successfully.",
+    data: answers,
+  });
 };
